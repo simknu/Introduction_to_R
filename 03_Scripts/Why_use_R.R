f4f29a0167d6cb11a597785b41def085ede39368
simknu <simon.knuesel@wsl.ch>
2019-09-10 14:06:42 +0200

    initial commit.

---
+++ 03_Scripts/Why_use_R.R
@@ -0,0 +1,14 @@
+
+# Why use R? ------------------------------------------------------------------
+
+# read data
+data_L1 <- read.csv("01_Data/data_L1.csv")
+
+# replace -99999 with NA
+data_L1$value[data_L1$value == -99999] <- NA
+
+# delete data in specified region
+data_L1$ts <- as.POSIXct(data_L1$ts, format = "%d.%m.%y %H:%M", tz = "UTC")
+data_L1$value[data_L1$ts >= "2019-06-01 14:50:00" & 
+                data_L1$ts <= "2019-06-01 17:00:00"] <- NA
+
