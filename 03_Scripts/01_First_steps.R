d1165dabaa0a99d1f2acd0f2a117a18191682ca2
simknu <simon.knuesel@wsl.ch>
2019-09-13 12:16:57 +0200

    initial commit

---
+++ 03_Scripts/01_First_steps.R
@@ -0,0 +1,32 @@
+
+# First steps in R ------------------------------------------------------------
+
+# clean working directory (remove all objects from workspace) ------
+#rm(list=ls())
+
+
+# Calculations with R ------
+# Addition
+5 + 3
+
+# Subtraction
+10 - 4
+
+
+# Variable assignment (objects) ------
+# assign value 42 to x
+x <- 42
+x
+
+# assign value 50 to y
+y <- 50
+Y # throws error because R is case sensitive
+y
+
+# calculate our_height (my_height + your_height)
+my_height <- 175
+your_height <- 180
+
+our_height <- my_height + your_height
+our_height
+
