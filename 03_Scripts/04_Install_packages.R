c1149f8ad89fbffcc8ab12705dfd4f9af5fbf621
simknu <simon.knuesel@wsl.ch>
2019-09-20 11:47:44 +0200

    initial commit

---
+++ 03_Scripts/04_Install_packages.R
@@ -0,0 +1,27 @@
+
+# Install packages ------------------------------------------------------------
+
+# Installation ----------
+install.packages("dplyr")
+
+# Load package
+library(dplyr) # after installation the packages have to be loaded to be available
+?dplyr # learn more about the package
+
+
+
+# More about packages ----------
+.libPaths() # path to packages on disk
+
+library() # list of all installed packages
+
+search() # list of all currently loaded packages
+
+# Press Ctrl + 7 to see the packages tab
+
+
+
+# Uninstall ----------
+remove.packages("dplyr") # uninstalls package from disk
+
+
