57176c1b23b1f2e1d02da92c29dd846ab88d904c
simknu <simon.knuesel@wsl.ch>
2019-10-02 10:29:00 +0200

    Changed formatting and added some comments.

--- 03_Scripts/07_Data_exploration.R
+++ 03_Scripts/07_Data_exploration.R
@@ -1,38 +1,53 @@
 
-# Data exploration ------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Title: Data exploration
 
-# Prepare workspace ----------
+# Description: Introduction how one can explore and summarise a dataset.
+#  1) Get an overview of the dataset
+#  2) Summary statistics
+#  3) Missing data
+#  4) Summary statistics by group
+
+# Author: Simon Knüsel
+
+# Date: 2019-09-30
+# -----------------------------------------------------------------------------
+
+
+# Prepare workspace --------------------
 #setwd("path_to_your_working_directory") # replace with your working directory
 #rm(list = ls())
 
-# load libraries
+# Load packages
 library(dplyr)
 
 
-# Load data ----------
+# Load data --------------------
+data(airquality)
+head(airquality)
 data(iris)
 head(iris)
 
-
-# Get an overview of data ----------
+# 1) Get an overview of the dataset --------------------
 View(iris) # see the dataset as a table
 
-dim(iris) # dimensions
+dim(iris) # dimensions (number of rows, number of columns)
 nrow(iris) # number of rows
 ncol(iris) # number of columns
 
 str(iris) # structure of the dataset
 
-# change column headers to lower case
+table(iris$Species) # number of occurrences by variable
+unique(iris$species) # unique instances by variable
+
+# change column headers (lowercase and underscore (_) instead of dots (.))
 colnames(iris)
-iris <- iris %>% 
-  rename(sepal_length = Sepal.Length, sepal_width = Sepal.Width,
-         petal_width = Petal.Width, petal_length = Petal.Length,
-         species = Species)
+colnames(iris) <- tolower(colnames(iris))
+colnames(iris) <- gsub("\\.", "_", colnames(iris)) # advanced: replace dots (.) with underscores (_)
 head(iris)
 
 
-# Summary statistics ----------
+# 2) Summary statistics --------------------
 summary(iris) # summary of dataset
 
 mean(iris$sepal_length)
@@ -50,20 +65,21 @@ var(iris$sepal_length) # variance
 quantile(iris$sepal_length) # quantiles
 
 
-# Missing data ----------
-data(airquality)
-head(airquality)
+# 3) Missing data --------------------
 
-mean(airquality$Ozone) # result is NA because of missing data (NA = not available)
-mean(airquality$Ozone, na.rm = TRUE) # result without NA (na.rm = remove NA's)
+# In R missing data should always be converted to "NA" (NA = not available)
+# Empty cells from a .csv file are usually automatically converted to NA
 
-# the argument 'na.rm' exists in many functions
+mean(airquality$Ozone) # result is NA because of missing data
+mean(airquality$Ozone, na.rm = TRUE) # result without NA (na.rm = remove NA's before calculation)
+
+# the argument "na.rm" exists in many functions
 median(airquality$Ozone, na.rm = TRUE)
 sd(airquality$Ozone, na.rm = TRUE)
 max(airquality$Ozone, na.rm = TRUE)
 
 
-# Summary statistics by group ----------
+# 4) Summary statistics by group --------------------
 table(iris$species) # number of occurrences by group (here species)
 
 # calculate mean, maximum and median of sepal length for each species
