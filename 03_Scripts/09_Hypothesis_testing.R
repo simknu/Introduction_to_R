7ae804dea62d5f6f569a4264c81fb1162fa39540
simknu <simon.knuesel@wsl.ch>
2019-10-02 10:29:41 +0200

    Changed formatting.

--- 03_Scripts/09_Hypothesis_testing.R
+++ 03_Scripts/09_Hypothesis_testing.R
@@ -1,41 +1,60 @@
 
-# Hypothesis testing ----------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Title: Hypothesis testing
 
-# Source: Introcuction to R by Jan Wunder (© Jan Wunder, 2016)
+# Description: Short introduction to hypothesis testing and linear models in R.
+#  This script shows some commands that can be used for hypothesis testing 
+#  or linear models, but it does not give the statistical background to these 
+#  functions. Please refer to statistical help for this.
+#  1) General workflow
+#  2) Two sample tests
+#  3) Linear models
+
+# Author: Simon Knüsel, based on scripts of Jan Wunder
+
+# Copyright: © Jan Wunder (2016)
+
+# Date: 2019-09-30
+# -----------------------------------------------------------------------------
 
 
-# General workflow ----------
-# 1. Formulate hypothesis
-# 2. Perform the experiment / Collect data
-# 3. Explore data
-# 4. Test hypothesis with appropriate test (check test assumptions!)
-
-
-# Prepare workspace ----------
+# Prepare workspace --------------------
 #setwd("path_to_your_working_directory") # replace with your working directory
 #rm(list = ls())
 
+# Load data --------------------
+data(airquality)
+head(airquality)
 
-# Two sample tests ------------------------------------------------------------
+
+# 1) General workflow --------------------
+#    1. Formulate hypothesis
+#    2. Perform the experiment / Collect data
+#    3. Explore data
+#    4. Test hypothesis with appropriate test (check test assumptions!)
+
+
+# 2) Two sample tests --------------------
+
 # Test whether two group means are the same
 
-# Two sample t-test ----------
+
+# Two sample t-test ------
+
 # Test assumptions: 
 # i) independent groups (paired = FALSE): data is normally distributed
 #    (+ equal variances)
 # ii) dependent groups (paired = TRUE): data is normally distributed
 
-
-# 1. Null Hypothesis: Basketball players are as tall as other people
-
-# 2. Collect data
+#    1. Null Hypothesis: Basketball players are as tall as other people
+#    2. Collect data
 basketball <- c(196, 195, 213, 170, 197, 204, 220, 210, 197) # height in cm
 other <- c(183, 179, 174, 172, 193, 163, 183, 201, 181) # height in cm
 
-# 3. Explore data
+#    3. Explore data
 boxplot(basketball, other, names = c("Basketball", "Other"))
 
-# 4. Test hypothesis (two sample t-test, independent groups)
+#    4. Test hypothesis (two sample t-test, independent groups)
 t.test(x = basketball, y = other, alternative = "two.sided", paired = FALSE)
 
 # Check test assumptions
@@ -43,29 +62,28 @@ qqnorm(basketball)
 qqline(basketball)
 qqnorm(other)
 qqline(other)
-# influence of sample size
-qqnorm(rnorm(10)) # sample 10 observations from normal distribution
-qqline(rnorm(10))
-qqnorm(rnorm(1000)) # sample 1000 observations from normal distribution
-qqline(rnorm(1000))
+# Influence of sample size
+basketball_small <- rnorm(n = 10) # randomly sample from normal distribution
+qqnorm(basketball_small)
+qqline(basketball_small)
+
+basketball_large <- rnorm(n = 1000)
+qqnorm(basketball_large)
+qqline(basketball_large)
 
 
-# Wilcoxon test ----------
+# Wilcoxon test ------
+
 # Test assumptions: 
 # i) independent groups (paired = FALSE): groups have the same distribution
 # ii) dependent groups (paired = TRUE): groups have the same distribution
 
-# 4. Test hypothesis (wilcoxon text, independent groups)
+#    4. Test hypothesis (wilcoxon text, independent groups)
 wilcox.test(x = basketball, y = other, alternative = "two.sided", 
             paired = FALSE)
 
 
-
-# Linear models ---------------------------------------------------------------
-
-# Load data
-data(airquality)
-head(airquality)
+# 3) Linear models --------------------
 
 # Relationship between wind and temperature
 plot(data = airquality, Wind ~ Temp)
@@ -78,5 +96,5 @@ abline(linear_model, col = "blue")
 
 # diagnostic plots
 plot(linear_model)
-?plot.lm
+?plot.lm # more information on the diagnostic plots
 
