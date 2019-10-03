80fe3547e24097a15ff91432a297c498b16c3a8c
simknu <simon.knuesel@wsl.ch>
2019-10-03 21:28:00 +0200

    restructured and renamed removed too complicated examples

--- 03_Scripts/05_Vector_data_frame_list.R
+++ 03_Scripts/05_Vector_data_frame_list.R
@@ -3,165 +3,121 @@
 # Title: Data types and structures
 
 # Description: Introduction to data types and structures.
-#  1) Data types (e.g. numeric, character, logical, etc.)
-#  2) Data structures (i.e vector, data frame, list)
+#  1) Vector
+#  2) Data frame
+#  3) List
 
 # Author: Simon Knüsel
 
-# Date: 2019-09-30
+# Date: 2019-10-07
 # -----------------------------------------------------------------------------
 
 
-# 1) Data types --------------------
+# 1) Vector --------------------
+
+# Vector classes ------
 
 # numeric
-x <- 55
-x
-class(x) # displays the data type of an object
+vec <- c(2, 5, 7, 9, 3.5)
+vec
+class(vec) # class() returns the class of an object
 
-
-# character
-city <- "Zurich"
+# character (characters or text need quotation marks "")
+city <- c("Zurich", "Basel", "Bern", "Geneva")
+City # throws error because R is case sensitive!
 city
 class(city)
 
-
-# logical
-good_Naming <- FALSE
-class(good_naming) # R is case sensitive (only use lowercase in object names)
-
-good_naming <- TRUE
-class(good_naming)
-
-
-# factor
-city
-class(city)
-city_fac <- as.factor(city) # classes can be converted
-class(city_fac)
-
+# logical (contains only TRUE or FALSE)
+male <- c(TRUE, FALSE, TRUE, TRUE, FALSE)
+male
+class(male)
 
 # date
 start <- "2019-02-03"
 end <- "2019-06-01"
-
-start <- as.Date(start, format = "%Y-%m-%d") # covert from character to date
 class(start)
-end <- as.Date(end, format = "%Y-%m-%d")
+class(end)
 
-end - start # calculations are possible with dates, difference in days
-
-
-# datetime
-start <- "2019-09-10 12:30"
-end <- "2019-09-13 22:37:35"
-
-start <- as.POSIXct(start, format = "%Y-%m-%d %H:%M")
+start <- as.Date(start) # classes can be convertet -> conversion from character to date
 class(start)
-end <- as.POSIXct(end, format = "%Y-%m-%d %H:%M:%S")
+end <- as.Date(end)
 
-end - start # difference in days
-difftime(time1 = end, time2 = start, units = "secs") # custom difference, here seconds
-
-
-# 2) Data structures --------------------
-
-# Vector ----------
-
-# vectors are objects with multiple elements
-vec_char <- c("Zurich", "Bern", "Geneva", "Lucerne", "Solothurn")
-vec_log <- c(TRUE, FALSE, TRUE, TRUE, FALSE)
-vec_num <- c(1, 5, 8, 8:13, 2, 4.6)
-vec_num
-
-# what data type is vec_num?
-class(vec_num)
-
-# calculate the mean of vec_1
-vec_1 <- seq(from = 2, to = 14, by = 2)
-vec_1
-
-?mean
-vec_1_mean <- mean(x = vec_1)
-vec_1_mean
-
-# what is the sum of vec_2?
-vec_2 <- c(TRUE, TRUE, FALSE, FALSE, TRUE)
-sum(vec_2) # TRUE = 1, FALSE = 0
+end - start # calculations are possible with objects of class date
 
 
-# select elements of vectors
-vec_char[2] # 2nd element
-vec_char[3:5] # elements 3-5
-vec_num[c(2, 5, 8)] # element 2, 5 and 8
+# Vector indexing ------
+## Select elements of a vector
+
+city[2] # 2nd element
+
+vec[3:5] # elements 3, 4 and 5
+
+male[c(2, 5, 8)] # elements 2, 5 and 8
 
 
-# change data types of vectors
-vec_char <- as.factor(vec_char)
-class(vec_char)
 
-vec_num <- as.character(vec_num)
-class(vec_num)
-sum(vec_num) # error because sum needs a numeric input
+# 2) Data frame --------------------
 
+# Load internal dataset airquality
+data(airquality)
 
-# create empty vector
-vec_3 <- vector(mode = "logical", length = 5) # empty logical vector
-vec_3
-vec_4 <- vector(mode = "numeric", length = 3) # empty numeric vector
-vec_4
-
-
-# Data frame ----------
-
-# use internal dataset airquality
-data(airquality) # load dataset
-head(airquality) # view first 6 lines of data frame
-View(airquality) # view dataset
-
-# assign airquality to df
+# Assign airquality to df
 df <- airquality
 class(df)
 
-# show the first 6 lines of the data frame
-head(df)
-# see the structure of the data frame
+# See structure of df (data frame consists of vectors)
 str(df)
 
 
-# select elements of a data frame
-df$Temp # select column by name
-df[, 4] # select column by number
-identical(df$Temp, df[, 4]) # selections are the same
+# Data frame indexing ------
+## Select parts of a data frame
 
-df[2, ] # select row by number
-df[2:6, ] # select multiple rows
+# Select with row and column numbers - df[row, col]
+df[1, 2] # first row, second column
 
-df[1:2, c(1, 3)] # select first two rows of columns 1 and 3
+df[, 2] # second column
 
+df[1:3, ] # first three rows
 
-# data frame has names (i.e. column headers)
-names(df) # general name of an object
-colnames(df) # column names
-
-# create a data frame from scratch
-df <- data.frame(First_Name = c("Eva", "Max", "Bill", "Susan"),
-                 Last_Name = c("Miller", "Moritz", "Walsh", "Bunn"),
-                 Age = c(23, 44, 58, 76))
-str(df)
+df2 <- df[1:3, 3:5] # rows 1:3, columns 3:5
+df2 # check the result
 
 
-# List ----------
+# Select by column name - df$column_name
+df$Wind
 
-# lists can contain multiple objects
-list <- list(df, airquality, vec_1, vec_2, vec_char)
+wind <- df$Wind
+class(wind) # numeric vector
+
+temp <- df$Temp
+df[, 4]
+identical(temp, df[, 4])
+
+
+# Column names ------
+colnames(df) # column names (i.e. header of the data frame)
+
+# Change column names
+colnames(df) <- c("ozone", "solar.r", "wind", "temp", "month", "day")
+colnames(df)
+head(df) # look at the first six rows of the data frame
+
+
+
+# 3) List --------------------
+
+# Lists can objects of different classes (i.e. data frames, vectors, ...)
+list <- list(df, airquality, city, vec, male)
 class(list)
 
 # select elements of a list
+list[1] # first list element, contains df
+list1
+
+list[[1]] # first list object
 l1 <- list[[1]]
-identical(df, l1)
+class(l1) # first list object is df, therefore its class is data.frame
 
-l2 <- list[1] # list that contains an object, here df
-
-l3 <- list[[4]][1:3] # select single elements from list object
-
+l3 <- list[[4]][1:3] # select single elements from list objects
+l3
