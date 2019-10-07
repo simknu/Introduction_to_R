bccdbcd875631da9a1da0a66a47cbdd9cd2800e0
simknu <simon.knuesel@wsl.ch>
2019-10-07 13:52:57 +0200

    initial commit.

---
+++ 04_Scripts_participants/01_First_steps_part.R
@@ -0,0 +1,48 @@
+
+# -----------------------------------------------------------------------------
+# Title: First steps in R
+
+# Description: This script demonstrates some very basic functionalities and 
+#  concepts of R
+#  1) Calculations with R
+#  2) Objects
+#  3) Functions
+
+# Author: Simon Knüsel
+
+# Date: 2019-10-07
+# -----------------------------------------------------------------------------
+
+
+# 1) Calculations with R --------------------
+
+
+
+
+
+# 2) Objects --------------------
+
+## To assign a value to an object always use "<-"
+
+
+
+
+
+
+# Object names ------
+## Object names should always be in lower case, connected words should be 
+## separated with an underscore (_)
+
+
+
+
+
+# 3) Functions --------------------
+## See also documentation in the 02_Slides folder
+
+## Functions take inputs via arguments, transform the input(s) and 
+## return an output
+## Functions have mandatory and optional arguments
+
+
+
