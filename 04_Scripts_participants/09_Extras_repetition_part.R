266db6fa02e8fa7b137bb650a6b78a78013a6a07
simknu <simon.knuesel@wsl.ch>
2019-11-06 09:18:08 +0100

    initial commit. Repetition to write along

---
+++ 04_Scripts_participants/09_Extras_repetition_part.R
@@ -0,0 +1,45 @@
+
+# -----------------------------------------------------------------------------
+# Title: Extras and repetition
+
+# Description: This script contains the repetition at the beginning of the
+#  second course day and answers to questions asked during the course.
+#  1) Repetition
+#  2) Extras
+
+# Author: Simon Knüsel
+
+# Date: 2019-11-06
+# -----------------------------------------------------------------------------
+
+
+# 1) Repetition --------------------
+
+# Vectors ------
+
+
+
+
+# Data frames ------
+
+# load data
+
+
+
+# get an overview of a data frame
+
+
+
+# add a new column
+
+
+
+# filter rows by condition
+
+
+
+# select parts of a data frame
+
+
+
+
