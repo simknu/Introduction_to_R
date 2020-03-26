bd120a49234f4e3f0b26e5b2bec2688c9c2db914
simknu <simon.knuesel@wsl.ch>
2020-03-26 15:15:10 +0100

    updated script to match new video tutorials

--- 03_Scripts/03_Load_and_save_data.R
+++ 03_Scripts/03_Load_and_save_data.R
@@ -15,9 +15,9 @@
 
 
 # 1) Set working directory --------------------
-## See also documentation in the 02_Slides folder
+## See also video tutorial in the '02_Slides_tutorials' folder
 
-# set working directory to the course folder "R_Basic_Introduction"
+# set working directory to the folder '01_Data' in the course folder 'R_Basic_Introduction'
 setwd("path_to_your_working_directory") # replace with your working directory
 getwd() # get the current working directory
 
@@ -25,11 +25,11 @@ getwd() # get the current working directory
 # 2) Load data (load a csv-File) --------------------
 
 # load file tree_growth_data.csv and assign it to tree_growth
-tree_growth <- read.csv(file = "01_Data/tree_growth_data.csv")
+tree_growth <- read.csv(file = "tree_growth_data.csv")
 head(tree_growth) # see first six lines of dataset
 
 # load file tree_growth_data_2.csv and assign it to tree_growth_2
-tree_growth_2 <- read.csv(file = "01_Data/tree_growth_data_2.csv", 
+tree_growth_2 <- read.csv(file = "tree_growth_data_2.csv", 
                           sep = ";") # use "sep = " to change separtor from comma (,) to semicolon (;)
 head(tree_growth_2)
 
@@ -37,6 +37,6 @@ head(tree_growth_2)
 # 3) Save data (save as a csv-File) --------------------
 
 # save as .csv (do not forget the file extension .csv in the file name!)
-write.csv(x = tree_growth, file = "01_Data/tree_growth_new.csv", 
+write.csv(x = tree_growth, file = "tree_growth_new.csv", 
           row.names = FALSE)
 
