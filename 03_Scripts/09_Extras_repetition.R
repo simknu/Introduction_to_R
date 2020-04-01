6027f607c81b4aa7290e4f2c284139fc384aaa0d
simknu <simon.knuesel@wsl.ch>
2020-04-01 16:03:29 +0200

    updated extras for course #1 in FS2020

--- 03_Scripts/09_Extras_repetition.R
+++ 03_Scripts/09_Extras_repetition.R
@@ -49,7 +49,7 @@ sepal_length
 
 # 2) Extras --------------------
 
-# Calculate the mean of rows
+### Calculate the mean of rows
 head(iris)
 iris_rowmean <- rowMeans(iris[, 1:4]) # select all numeric columns (1:4) of the iris dataset
 iris_rowmean
@@ -58,4 +58,98 @@ iris_rowmean
 iris_mean <- iris
 iris_mean$rowmean <- rowMeans(iris_mean[, 1:4])
 head(iris_mean)
+
+
+
+### Overlay histograms
+
+# prepare datasets for the two histograms
+iris_versicolor <- subset(iris, Species == "versicolor")
+iris_virginica <- subset(iris, Species == "virginica")
+
+# Plot the first histogram
+# Set the axis limits (for both histograms) with 'xlim' for the 
+# x-axis and ylim for the y-axis
+hist(x = iris_versicolor$Petal.Length, xlab = "Petal length",
+     main = "Overlapping histograms", xlim = c(2, 8), ylim = c(0, 25), 
+     col = "blue")
+
+# Add the second histogram
+# With the argument 'add' the histogram is added to the first plot
+hist(x = iris_virginica$Petal.Length, col = "red", add = TRUE)
+
+
+# To make the colors transparent you need the function 'rgb'
+# You can set the transparency inside the 'rgb' function
+# as the fourth number, i.e. rgb(1, 2, 3, 0.5) sets the 
+# transparency to 0.5 (to 50%)
+hist(x = iris_versicolor$Petal.Length, xlab = "Petal length",
+     main = "Overlapping histograms", xlim = c(2, 8), ylim = c(0, 25), 
+     col = rgb(0, 0, 1, 0.5))
+hist(x = iris_virginica$Petal.Length, col = rgb(1, 0, 0, 0.5), 
+     add = TRUE)
+
+
+
+
+
+### Save plots as an object
+
+# Apparently you cannot save plots produced with the 'plot'
+# function to an object. So you need to use the structure
+# I showed you in the file '07_Data_visualisation.R' to 
+# save the plot. Or you can use the file menu with the 
+# 'Export'-button in the 'Plots'-tab on the bottom right.
+
+# Load the dataset
+data(iris)
+# Prepare the iris_setosa dataset
+iris_setosa <- subset(iris, Species == "setosa")
+
+# 1) Define the name and size of the plot and where to save it
+pdf(file = "my_first_histogram.pdf", width = 8, height = 4)
+
+# 2) Draw plot(s) (i.e. run plot commands)
+hist(x = iris_setosa$Petal.Length, xlab = "Petal length", 
+     main = "Iris setosa", xlim = c(0.75, 2), ylim = c(0, 15))
+
+# 3) Write plot to file by shutting off the plotting device
+dev.off()
+
+
+
+# However, you can save plots produced with other functions.
+# For example with 'ggplot2'. For more examples with this function
+# I recommend you the R-course on 'tidyverse' at UZH. Or you can
+# also have a look at the R cheat sheet on 'ggplot2'
+# (https://github.com/rstudio/cheatsheets/raw/master/data-visualization-2.1.pdf).
+
+# This part is now more advanced than what we covered in the course.
+# You do not need to get nervous if you do not understand all of the
+# code below.
+
+# To use ggplot2 you first need to install the package 'ggplot'
+install.packages("ggplot2") # You only need to install the package the first time
+
+# Afterwards you need to load the package
+library(ggplot2) # You always need to load packages at the start of every session (i.e. after you restart RStudio)
+
+# After loading the library you can start with plotting
+ggplot(data = iris_virginica, aes(x = Petal.Length)) +
+  geom_histogram(bins = 20) +
+  xlim(3, 8) +
+  ylim(0, 15)
+
+# Note: The error you get is only a warning message that 4 of the bars
+# do not have any observation (count = 0)
+
+# You can save this plot to an object
+ggplot_1 <- ggplot(data = iris_virginica, aes(x = Petal.Length)) +
+  geom_histogram(binwidth = 0.2)
+
+ggplot_1 # The plot is only plotted once you run the saved object
+
+# Now you can save the plot with the 'ggsave' function
+ggsave(ggplot_1, filename = "my_first_ggplot.pdf", height = 4, width = 8)
+
 
